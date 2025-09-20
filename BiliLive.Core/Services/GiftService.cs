9ccb4cd67716566fc0cf0cc2b5c2c33b6b82db1b
geruipu <greepar@outlook.com>
2025-09-20 21:49:15 +0800

    送礼和发送弹幕初步框架

---
+++ BiliLive.Core/Services/GiftService.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace BiliLive.Core.Services;
+
+public class GiftService : IDisposable
+{
+    private const string UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0";
+
+    private readonly HttpClient _httpClient;
+    private readonly CookieContainer _cookieContainer= new ();
+    private bool _disposed;
+    
+    public GiftService(string biliCookie,string? proxyAddress = null,string? username = null,string? password = null)
+    {
+        //添加Cookie
+        var cookiePairs = biliCookie.Split(';');
+        foreach (var pair in cookiePairs)
+        {
+            var cookieParts = pair.Split('=', 2);
+            if (cookieParts.Length != 2) continue;
+
+            var name = cookieParts[0].Trim();
+            var value = cookieParts[1].Trim();
+
+            _cookieContainer.Add(new Cookie(name, value)
+            {
+                Domain = ".bilibili.com",
+                Path = "/"
+            });
+        }
+        var handler = new HttpClientHandler
+        {
+            UseCookies = true,
+            CookieContainer = _cookieContainer,
+        };
+        
+        if (!string.IsNullOrWhiteSpace(proxyAddress))
+        {
+            var proxy = new WebProxy(proxyAddress);
+
+            if (!string.IsNullOrWhiteSpace(username))
+            {
+                proxy.Credentials = new NetworkCredential(username, password);
+            }
+
+            handler.UseProxy = true;
+            handler.Proxy = proxy;
+            
+            _httpClient = new HttpClient(handler, disposeHandler: true);
+        }
+        else
+        {           
+            _httpClient = new HttpClient(handler, disposeHandler: true);
+        }
+        _httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(UserAgent);
+
+    }
+ 
+    
+    public async Task SendDanmakuAsync()
+    { 
+        var responseStinga = await _httpClient.GetAsync(new Uri("https://4.ipw.cn"));
+        var responseContentb = await responseStinga.Content.ReadAsStringAsync();
+        Console.WriteLine(responseContentb);
+        
+        var formData = new Dictionary<string, string>
+        {
+            { "bubble", "0"  },
+            { "msg", "主播您好，祝您直播愉快！" },
+            { "color", "16777215"  },
+            { "mode", "1"  },
+            { "room_type", "0"  },
+            { "jumpfrom", "0"  },
+            { "reply_mid", "0"  },
+            { "reply_attr", "0"  },
+            { "replay_dmid", ""  },
+            { "statistics", "{\"appId\":100,\"platform\":5}"  },
+            { "reply_type", "0"  },
+            { "reply_uname", ""  },
+            { "data_extend", "{\"trackid\":\"-99998\"}"  },
+            { "fontsize", "25"  },
+            { "rnd", ((int)(DateTimeOffset.UtcNow.ToUnixTimeSeconds())).ToString()  },
+            { "roomid", "10431980"  },
+            { "csrf", GetCsrfFromCookie() ?? ""  },
+            { "csrf_token", GetCsrfFromCookie() ?? ""  }
+        };
+        
+       
+        var responseSting = await _httpClient.PostAsync(new Uri(" https://api.live.bilibili.com/msg/send"),new FormUrlEncodedContent(formData));
+        var responseContent = await responseSting.Content.ReadAsStringAsync();
+        Console.WriteLine(responseContent);
+    }
+    
+    public async Task SendGiftAsync()
+    { 
+        var responseStinga = await _httpClient.GetAsync(new Uri("https://4.ipw.cn"));
+        var responseContentb = await responseStinga.Content.ReadAsStringAsync();
+        Console.WriteLine(responseContentb);
+        
+        var formData = new Dictionary<string, string>
+        {
+            { "uid", "3493081477286220"  },//自己的uid
+            { "gift_id", "31039" },//礼物ID
+            { "ruid", "196431435"  },//主播的uid
+            { "send_ruid", "0"  },
+            { "gift_num", "1"  },
+            { "coin_type", "gold"  },
+            { "bag_id", "0"  },
+            { "platform", "pc"  },
+            { "biz_code", "Live"  },
+            { "biz_id", "10431980"  },
+            { "storm_beat_id", "0"  },
+            { "metadata", ""  },
+            { "price", "100"  },
+            { "receive_users", ""  },
+            { "live_statistics", "{\"pc_client\":\"pcWeb\",\"jumpfrom\":\"-99998\",\"room_category\":\"0\",\"source_event\":0,\"trackid\":\"-99998\",\"official_channel\":{\"program_room_id\":\"-99998\",\"program_up_id\":\"-99998\"}}"  },
+            { "statistics", "{\"platform\":5,\"pc_client\":\"pcWeb\",\"appId\":100}"  },
+            { "web_location", "444.8"  },
+            { "csrf", GetCsrfFromCookie() ?? ""  },
+            { "csrf_token", GetCsrfFromCookie() ?? ""  },
+            { "wts", ((int)(DateTimeOffset.UtcNow.ToUnixTimeSeconds())).ToString()  }
+        };
+       
+        var (imgKey, subKey) = await GetWbiKeys();
+        Dictionary<string, string> signedParams = EncWbi(
+            parameters: formData,
+            imgKey: imgKey,
+            subKey: subKey
+        );
+
+        string query = await new FormUrlEncodedContent(signedParams).ReadAsStringAsync();
+
+        Console.WriteLine(query);
+       
+        var responseSting = await _httpClient.PostAsync(new Uri(" https://api.live.bilibili.com/xlive/revenue/v1/gift/sendGold"),new StringContent(query, Encoding.UTF8, "application/x-www-form-urlencoded"));
+        
+        var responseContent = await responseSting.Content.ReadAsStringAsync();
+        Console.WriteLine(responseContent);
+        // var responseSting = await _httpClient.PostAsync(new Uri(" https://api.live.bilibili.com/msg/send"),new FormUrlEncodedContent(formData));
+        // var responseContent = await responseSting.Content.ReadAsStringAsync();
+        // Console.WriteLine(responseContent);
+    }
+    
+    //test
+    // public async Task GetAccInfoAsync()
+    // {
+    //     var checkLoginApi = "https://api.bilibili.com/x/web-interface/nav";
+    //     var responseString = await _httpClient.GetStringAsync(checkLoginApi);
+    //     Console.WriteLine(responseString);
+    // }
+    
+    private string? GetCsrfFromCookie()
+    {
+        var cookies = _cookieContainer.GetCookies(new Uri("https://space.bilibili.com"));
+        var cookie = cookies["bili_jct"];
+        return cookie?.Value;
+    }
+    
+    
+    // 算法
+    private static readonly int[] MixinKeyEncTab =
+    [
+        46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49, 33, 9, 42, 19, 29, 28, 14, 39,
+        12, 38, 41, 13, 37, 48, 7, 16, 24, 55, 40, 61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63,
+        57, 62, 11, 36, 20, 34, 44, 52
+    ];
+
+    //对 imgKey 和 subKey 进行字符顺序打乱编码
+    private static string GetMixinKey(string orig)
+    {
+        return MixinKeyEncTab.Aggregate("", (s, i) => s + orig[i])[..32];
+    }
+
+    private static Dictionary<string, string> EncWbi(Dictionary<string, string> parameters, string imgKey,
+        string subKey)
+    {
+        string mixinKey = GetMixinKey(imgKey + subKey);
+        string currTime = DateTimeOffset.Now.ToUnixTimeSeconds().ToString();
+        //添加 wts 字段
+        parameters["wts"] = currTime;
+        // 按照 key 重排参数
+        parameters = parameters.OrderBy(p => p.Key).ToDictionary(p => p.Key, p => p.Value);
+        //过滤 value 中的 "!'()*" 字符
+        parameters = parameters.ToDictionary(
+            kvp => kvp.Key,
+            kvp => new string(kvp.Value.Where(chr => !"!'()*".Contains(chr)).ToArray())
+        );
+        // 序列化参数
+        string query = new FormUrlEncodedContent(parameters).ReadAsStringAsync().Result;
+        //计算 w_rid
+        using MD5 md5 = MD5.Create();
+        byte[] hashBytes = md5.ComputeHash(Encoding.UTF8.GetBytes(query + mixinKey));
+        string wbiSign = BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+        parameters["w_rid"] = wbiSign;
+
+        return parameters;
+    }
+
+    // 获取最新的 img_key 和 sub_key
+      private static async Task<(string, string)> GetWbiKeys()
+      {
+          var httpClient = new HttpClient();
+          httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36");
+          httpClient.DefaultRequestHeaders.Referrer = new Uri("https://www.bilibili.com/");
+      
+          HttpResponseMessage responseMessage = await httpClient.SendAsync(new HttpRequestMessage
+          {
+              Method = HttpMethod.Get,
+              RequestUri = new Uri("https://api.bilibili.com/x/web-interface/nav"),
+          });
+      
+          JsonNode response = JsonNode.Parse(await responseMessage.Content.ReadAsStringAsync())!;
+      
+          string imgUrl = (string)response["data"]!["wbi_img"]!["img_url"]!;
+          imgUrl = imgUrl.Split("/")[^1].Split(".")[0];
+      
+          string subUrl = (string)response["data"]!["wbi_img"]!["sub_url"]!;
+          subUrl = subUrl.Split("/")[^1].Split(".")[0];
+          return (imgUrl, subUrl);
+      }
+
+      //算法
+    
+    public void Dispose()
+    {
+        if (!_disposed)
+        {
+            _httpClient.Dispose();
+            _disposed = true;
+        }
+    }
+}
--- BiliLive/Views/MainWindow/Pages/AutoService/AutoServiceViewModel.cs
+++ BiliLive/Views/MainWindow/Pages/AutoService/AutoServiceViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Avalonia.Controls;
 using Avalonia.Media;
+using BiliLive.Core.Interface;
 using BiliLive.Core.Services;
+using BiliLive.Core.Services.BiliService;
 using BiliLive.Models;
 using BiliLive.Resources;
 using BiliLive.Services;
@@ -9,6 +12,7 @@ using BiliLive.Utils;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace BiliLive.Views.MainWindow.Pages.AutoService;
 
@@ -26,6 +30,22 @@ public partial class AutoServiceViewModel : ViewModelBase
     [ObservableProperty] private string? _startHour;
     [ObservableProperty] private string? _startMinute;
     [ObservableProperty] private string? _startSecond;
+
+    private readonly IBiliService _biliService;
+ 
+    
+    public AutoServiceViewModel(IServiceProvider? serviceProvider = null)
+    {
+        if (Design.IsDesignMode)
+        {
+            //设计时数据
+            _biliService = new BiliServiceImpl();
+        }
+        else
+        {
+            _biliService = serviceProvider!.GetRequiredService<IBiliService>();
+        }
+    }
     
     [RelayCommand]
     private async Task ToggleOptions()
@@ -39,15 +59,8 @@ public partial class AutoServiceViewModel : ViewModelBase
                       minute * 60 +
                       (IsRandomSecond ? random.Next(-240, -120) : second);
         
-        
         //设置第二天基准
         var streamTime = DateTime.Today.AddDays(1).AddSeconds(seconds);
-        
-        
-        // WeakReferenceMessenger.Default.Send(new ShowNotificationMessage($"设置时间{streamTime}",Geometry.Parse(MdIcons.Check)));
-        
-          
-       
         
         var popupMsg = IsEnabled ? $"自动开播将在 {streamTime} 开始" : "已关闭自动开播服务";
         var icon = IsEnabled ? Geometry.Parse(MdIcons.Check) : Geometry.Parse(MdIcons.Error);
@@ -108,6 +121,16 @@ public partial class AutoServiceViewModel : ViewModelBase
         await ConfigManager.SaveConfigAsync(ConfigType.Check60MinTask,IsCheck60MinTask);
     }
     
-    
+    [RelayCommand]
+    private async Task TestAction()
+    {
+        await Task.Delay(1);
+        Console.WriteLine("test action");
+        var cookie = "CURRENT_QUALITY=0;b_lsid=3D9C828D_19966E7C879;theme-tip-show=SHOWED;home_feed_column=4;LIVE_BUVID=AUTO3917563974158518;buvid4=693757A4-F7AE-46F7-FD89-176D5864FF0465440-025080417-+Y68DEMsE7icBBnI4Ogsb8zMS4RLFD0sjE3sN1E7Y+p84Jp9uaGgje/3Dwd3EJRN;CURRENT_FNVAL=2000;buvid3=AAD37E52-3183-C9A7-8B21-A29BA301223E64690infoc;share_source_origin=COPY;sid=53fl154r;SESSDATA=921eeba8%2C1773900917%2C88c6b%2A91CjDngIQNz7HBaLpv599D40Uaey4Gl0GyJ09-mIbnwYoSaFDE_s3f1u5Es1naJ7zEBO4SVkZDdEFwdlJ2S1A2OHkzcEJoZUNPcm81cTlEem5WTWx6NTRLcm12QzJaRnpSRnlnR0VvWVJNRS15dWM5cjI4Q2Nnel9aUnNOdlhFWjFOVVBiZmlyZG13IIEC;bsource=share_source_copy_link;bmg_src_def_domain=i1.hdslb.com;theme-avatar-tip-show=SHOWED;b_nut=1754298164;_uuid=8F10F9DCF-3A93-1186-A31D-3E4D39CBACFB63157infoc;bili_jct=b262eb807ec125b7e6ac1c06c54aa26e;bili_ticket=eyJhbGciOiJIUzI1NiIsImtpZCI6InMwMyIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTg2MTYzOTMsImlhdCI6MTc1ODM1NzEzMywicGx0IjotMX0.axlnDqIVwLHxwg9wx1YZxgZP4fLVTLMefLVAf2IKf9I;bili_ticket_expires=1758616333;bmg_af_switch=1;bp_t_offset_3493081477286220=1114632377969147904;browser_resolution=1385-744;buvid_fp=4973860c9239ec9f36c712d0d6a5b2e3;DedeUserID=3493081477286220;DedeUserID__ckMd5=5625cdd7363cee29;enable_web_push=DISABLE;hit-dyn-v2=1;PVID=1;rpdid=|(u|u~J~mu)~0J'u~l)JR)mku";
+        using var giftService = new GiftService(cookie,"http://142.111.48.253:7030","pvcufsir","5ppw7212petm");
+        // await giftService.SendDanmakuAsync();
+        await giftService.SendGiftAsync();
+        // await giftService.GetAccInfoAsync();
+    }
     
 }
