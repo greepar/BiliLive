6ab4dab8025728d420ef0b638b9b3ca949bc05f9
geruipu <greepar@outlook.com>
2025-11-25 17:09:15 +0800

    sync

---
+++ Material3.UI/Material3Res.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.Styling;
+using Avalonia.Styling;
+
+namespace Material3.UI;
+
+public partial class Material3Res : ResourceInclude
+{
+    public Material3Res(Uri? baseUri) : base(baseUri)
+    {
+    }
+
+    public Material3Res(IServiceProvider serviceProvider) : base(serviceProvider)
+    {
+    }
+}
